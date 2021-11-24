161fa73114693a9a7135dcf3a7a5af5a2bdcd818
cihat <cihat02020202@gmail.com>
2021-11-24 17:10:27 +0300

    Update index routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { forwardAuthenticated, ensureAuthenticated } = require('../helpers/password/auth');
 
 const welcomeMessage = `
   <h1>Welcome to my Blog Api</h1>
@@ -11,13 +12,23 @@ const welcomeMessage = `
 `
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', welcomeMessage });
-  res.send({ welcomeMessage });
-});
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Express', welcomeMessage });
+//   res.send({ welcomeMessage });
+// });
 
 router.get("/api", (req, res) => {
   res.send({ welcomeMessage });
 })
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome', { title: 'Express', welcomeMessage }));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+);
 
 module.exports = router;
