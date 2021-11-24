03765a0512077e4563413d0019e55793e13106a9
cihat <cihat02020202@gmail.com>
2021-11-24 17:07:58 +0300

    Add passport and express-session for auth && Update routes

--- app.js
+++ app.js
@@ -3,16 +3,31 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const feetRouter = require('./routes/feed');
+
+require("./bootstrap")
+require("./helpers/password")(passport)
 
 const app = express();
-require("./bootstrap")
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(session({
+  secret: "thisisaveryverysecretkey",
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,7 +36,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
+app.use("/posts", postsRouter)
+app.use("/feed", feetRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
